65bf023643926c8d528bb04866fe3bbef5fa3a76
Hans-Gunther Schmidt <hgs@cloudcontrol.de>
2012-02-28 16:35:10 +0100

    updating setup.py information

--- setup.py
+++ setup.py
@@ -1,14 +1,55 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+    Copyright (c) 2012 cloudControl GmbH
 
-from setuptools import setup
+    Permission is hereby granted, free of charge, to any person
+    obtaining a copy of this software and associated documentation
+    files (the "Software"), to deal in the Software without
+    restriction, including without limitation the rights to use,
+    copy, modify, merge, publish, distribute, sublicense, and/or
+    sell copies of the Software, and to permit persons to whom the
+    Software is furnished to do so, subject to the following
+    conditions:
+
+    The above copyright notice and this permission notice shall be
+    included in all copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+    EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+    OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+    NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+    HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+    WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+    FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+    OTHER DEALINGS IN THE SOFTWARE.
+
+    setup script DoozerLib
+
+    usage: sudo python setup.py install
+"""
+
+from distutils.core import setup
+from setuptools import find_packages
+import version
+
+DOWNLOAD_URL = 'https://github.com/downloads/cloudControl/pydoozer/pydoozer-{0}.tar.gz'.format(version.__version__)
+
+extra_options = dict()
 
 setup(
-    name='PyDoozer',
-    version='0.1.0',
-    author='Jeff Lindsay',
-    author_email='jeff.lindsay@twilio.com',
-    description='doozer client',
-    packages=['doozer'],
+    name="PyDoozer",
+    version=version.__version__,
+    description='PyDoozer - Doozer client library for Python. Based on github.com/progrium/pydoozer.',
+    author='cloudControl Team',
+    author_email='info@cloudcontrol.de',
+    url='https://github.com/cloudControl/pydoozer',
     install_requires=['gevent', 'protobuf'],
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    scripts=[],
     data_files=[],
+    download_url=DOWNLOAD_URL,
+    license='Apache 2.0',
+    **extra_options
 )
